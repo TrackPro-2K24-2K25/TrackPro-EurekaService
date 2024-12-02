7022c42ed36cab98d68f0ba88ddd5738c6d3760c
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-12-02 19:44:10 +0100

    first commit

---
+++ src/test/java/org/youcode/trackproeurekaservice/TrackProEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youcode.trackproeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackProEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
